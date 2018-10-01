a3e1f1472b6212606fa866cbcf4d6f24bba0429a
Bios-Marcel <marceloschr@googlemail.com>
2018-10-01 22:20:12 +0200

    Add method to pass along the working directory of gta

--- sampcmd/sampcmd.c
+++ sampcmd/sampcmd.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 
-int _LaunchSAMP(wchar_t* args) {
-    int ret = 0;
+int _LaunchSAMPSetWorkingDir(wchar_t* working_dir, wchar_t* args) {
+ int ret = 0;
     HMODULE module_handle = GetModuleHandleW(L"kernel32.dll");
     FARPROC load_library_w_proc;
     LPVOID ptr; 
@@ -11,20 +11,16 @@ int _LaunchSAMP(wchar_t* args) {
     STARTUPINFOW startup_info;
     HANDLE rt;
 
-    wchar_t cur_dir[FILENAME_MAX + 1];
     wchar_t samp_dll[FILENAME_MAX + 1];
     wchar_t gta_sa_exe[FILENAME_MAX + 1];
 
     memset(&process_info, 0, sizeof(PROCESS_INFORMATION));
     memset(&startup_info, 0, sizeof(STARTUPINFOW));
 
-    memset(cur_dir, 0, sizeof(wchar_t) * (FILENAME_MAX + 1));
-    DWORD sz = GetCurrentDirectoryW(FILENAME_MAX, cur_dir);
-
-    memcpy(samp_dll, cur_dir, sizeof(wchar_t) * (sz+1));
+    memcpy(samp_dll, working_dir, sizeof(wchar_t) * (sz+1));
     wcscat(samp_dll, L"\\samp.dll");
 
-    memcpy(gta_sa_exe, cur_dir, sizeof(wchar_t) * (sz+1));
+    memcpy(gta_sa_exe, working_dir, sizeof(wchar_t) * (sz+1));
     wcscat(gta_sa_exe, L"\\gta_sa.exe");
 
     if (module_handle)
@@ -79,4 +75,12 @@ int _LaunchSAMP(wchar_t* args) {
     }
 
     return ret;
+}
+
+int _LaunchSAMP(wchar_t* args) {
+    wchar_t cur_dir[FILENAME_MAX + 1];
+    memset(cur_dir, 0, sizeof(wchar_t) * (FILENAME_MAX + 1));
+    DWORD sz = GetCurrentDirectoryW(FILENAME_MAX, cur_dir);
+
+    return _LaunchSAMPSetWorkingDir(&cur_dir, args);
 }
--- sampcmd/sampcmd.go
+++ sampcmd/sampcmd.go
@@ -3,6 +3,7 @@ package sampcmd
 /*
 #include <stddef.h>
 int _LaunchSAMP(wchar_t *args);
+int _LaunchSAMPSetWorkingDir(wchar_t *working_dir, wchar_t *args);
 */
 import "C"
 
@@ -11,8 +12,22 @@ import (
 )
 
 //LaunchSAMP launches the SA-MP client passing along the given string as its commandline parameters.
+//This has to be executed from within the GTA SA folder.
 func LaunchSAMP(args string) int {
-	wcharString, _ := gowchar.StringToWcharT(args)
-	argsAsWchar := (*C.wchar_t)(wcharString)
-	return int(C._LaunchSAMP(argsAsWchar))
+	argsAsWchar, _ := gowchar.StringToWcharT(args)
+	argsAsWcharCPointer := (*C.wchar_t)(argsAsWchar)
+
+	return int(C._LaunchSAMP(argsAsWcharCPointer))
+}
+
+//LaunchSAMPSetWorkingDir launches the SA-MP client passing along the given string as its commandline parameters.
+//You must pass the GTA SA folder as the working directory here.
+func LaunchSAMPSetWorkingDir(workingDir, args string) int {
+	argsAsWchar, _ := gowchar.StringToWcharT(args)
+	argsAsWcharCPointer := (*C.wchar_t)(argsAsWchar)
+
+	workingDirAsWchar, _ := gowchar.StringToWcharT(workingDir)
+	workingDirAsWcharCPointer := (*C.wchar_t)(workingDirAsWchar)
+
+	return int(C._LaunchSAMPSetWorkingDir(workingDirAsWcharCPointer, argsAsWcharCPointer))
 }
