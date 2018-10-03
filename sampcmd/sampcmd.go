4bc7cc9078fb7a3be42b740d8115c31d957dbe20
Bios-Marcel <marceloschr@googlemail.com>
2018-10-03 16:10:37 +0200

    Add method that automatically detects installation path

--- sampcmd/sampcmd.go
+++ sampcmd/sampcmd.go
@@ -8,8 +8,14 @@ int _LaunchSAMPSetWorkingDir(wchar_t *working_dir, wchar_t *args);
 import "C"
 
 import (
+	"path/filepath"
+
 	"github.com/orofarne/gowchar"
+	"golang.org/x/sys/windows/registry"
 )
+
+//RegistryError error used for problems during registry calls.
+const RegistryError = 12345
 
 //LaunchSAMP launches the SA-MP client passing along the given string as its commandline parameters.
 //This has to be executed from within the GTA SA folder.
@@ -30,4 +36,23 @@ func LaunchSAMPSetWorkingDir(workingDir, args string) int {
 	workingDirAsWcharCPointer := (*C.wchar_t)(workingDirAsWchar)
 
 	return int(C._LaunchSAMPSetWorkingDir(workingDirAsWcharCPointer, argsAsWcharCPointer))
+}
+
+//LaunchSAMPDetectGTADirectory launches the SA-MP client passing along the given string as its commandline parameters.
+//This automatically detects the location where GTA SA is installed.
+func LaunchSAMPDetectGTADirectory(args string) int {
+	argsAsWchar, _ := gowchar.StringToWcharT(args)
+	argsAsWcharCPointer := (*C.wchar_t)(argsAsWchar)
+
+	key, keyError := registry.OpenKey(registry.CURRENT_USER, "Software\\SAMP", registry.READ)
+	if keyError != nil {
+		return RegistryError
+	}
+
+	path, _, valueError := key.GetStringValue("gta_sa_exe")
+	if valueError != nil {
+		return RegistryError
+	}
+
+	return int(C._LaunchSAMPSetWorkingDir(filepath.Dir(path), argsAsWcharCPointer))
 }
